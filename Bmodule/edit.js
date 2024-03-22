33e4dd474535801d88ce70dc288f39cf772551bc
xodnd0507 <24457545yong@gmail.com>
2024-03-22 13:36:55 +0900

    feat : Add addBtn function

--- Bmodule/edit.js
+++ Bmodule/edit.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
     $("#addBtn").click(function(){
-        
-    })
+        var input = $("<input>").attr({type: "file", accept: "image/*"}).on("change", function(event){
+            var file = event.target.files[0];
+            if( file ){
+                var reader = new FileReader();
+                reader.onload = function(){
+                    $(".area").append($("<div>").addClass("imgContainer").append($("<img>").attr("src", reader.result)));
+                };
+                reader.readAsDataURL(file);
+            }
+        });
+        input.click();
+    });
 })
