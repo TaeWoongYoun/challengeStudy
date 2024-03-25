217eddeaad50d0df0d8145af502c477a301a7776
xodnd0507 <24457545yong@gmail.com>
2024-03-25 18:04:09 +0900

    test : Update study

--- Bmodule/edit.js
+++ Bmodule/edit.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
     $("#addBtn").click(function(){
-        var input = $("<input>").attr({type: "file", accept: "image/*"}).on("change", function(event){
+        var input = ("<input>").attr({type : "file", accept: "image/*"}).on("change", function(event){
             var file = event.target.files[0];
             if (file) {
                 var reader = new FileReader();
                 reader.onload = function(){
                     $(".area").append($("<div>").addClass("imgContainer").append($("<img>").attr("src", reader.result)));
-                };
+                }
                 reader.readAsDataURL(file);
             };
         });
@@ -14,14 +14,30 @@ $(document).ready(function(){
     });
 
     $("#textBtn").click(function(){
-        $(".textContainer").show();
+        $(".textCotainer").show();
     });
 
     $("#addTextBtn").click(function(){
         var text = $("#textBox").val().trim();
         if (text !== ""){
-            $(".imgContainer").append($("<p>").addClass("text-on-image").text(text));
+            $(".imgContainer").append($("<p>").addClass(".text-on-image").text(text));
             $(".textContainer").hide();
         }
+    });
+
+    $(document).on("mousedown", "text-on-image", function(event){
+        isDragging = true;
+        startX = event.clientX;
+        startY = event.clientY;
+        initialLeft = $(test).position().left;
+        initialTop = $(test).position().top;
+    }).on("mouseup", function(){
+        isDragging = false;
+    }).on("mousemove", function(event){
+        if(isDragging){
+            let offsetX = event.clientX - startX;
+            let offsetY = event.clientY - startY;
+            $(".text-on-image").css({left: initialLeft + offsetX + "px", top: initialTop + offsetY + "px"});
+        }
     })
-})
+});
