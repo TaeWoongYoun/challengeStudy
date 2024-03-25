ca703effe396e6cb7328ba5006358eaa6ef6416d
xodnd0507 <24457545yong@gmail.com>
2024-03-25 10:08:45 +0900

    test : Add textBtn function

--- Bmodule/edit.js
+++ Bmodule/edit.js
@@ -2,16 +2,26 @@ $(document).ready(function(){
     $("#addBtn").click(function(){
         var input = $("<input>").attr({type: "file", accept: "image/*"}).on("change", function(event){
             var file = event.target.files[0];
-            if(file){
+            if (file) {
                 var reader = new FileReader();
                 reader.onload = function(){
                     $(".area").append($("<div>").addClass("imgContainer").append($("<img>").attr("src", reader.result)));
                 };
                 reader.readAsDataURL(file);
-            }
-        })
+            };
+        });
         input.click();
     });
 
-    
+    $("#textBtn").click(function(){
+        $(".textContainer").show();
+    });
+
+    $("#addTextBtn").click(function(){
+        var text = $("#textBox").val().trim();
+        if (text !== ""){
+            $(".imgContainer").append($("<p>").addClass("text-on-image").text(text));
+            $(".textContainer").hide();
+        }
+    })
 })
