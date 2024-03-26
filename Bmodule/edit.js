66dff69e485280140b540388dc901cceeb025a8e
xodnd0507 <24457545yong@gmail.com>
2024-03-26 15:19:30 +0900

    test : Test study

--- Bmodule/edit.js
+++ Bmodule/edit.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $("#addBtn").click(function(){
         var input = $("<input>").attr({type: "file", accept: "image/*"}).on("change", function(event){
             var file = event.target.files[0];
-            if (file) {
+            if (file){
                 var reader = new FileReader();
                 reader.onload = function(){
                     $(".area").append($("<div>").addClass("imgContainer").append($("<img>").attr("src", reader.result)));
@@ -15,33 +15,46 @@ $(document).ready(function(){
 
     $("#textBtn").click(function(){
         $(".textContainer").show();
-    });
+    })
 
-    $("#addTextBtn").click(function() {
+    $("#addTextBtn").click(function(event){
         var text = $("#textBox").val().trim();
-        if (text !== "") {
+        if (text) {
             $(".imgContainer").append($("<p>").addClass("text-on-image").text(text));
             $(".textContainer").hide();
         }
     });
 
-    $("#rotateBtn").click(function(){
-        $(".text-on-=image").toggleClass("rotate");
+    $("rotateBtn").click(function(){
+        $(".text-on-image").toggleClass("rotate");
     });
 
     $(document).on("mousedown", ".text-on-image", function(event){
-        isDragging = true;
-        stratX = event.clientX;
-        stratY = event.clientY;
+        isDraggig = true;
+        startX = event.clientX;
+        startY = event.clientY;
         initialLeft = $(this).position().left;
         initialTop = $(this).position().top;
     }).on("mouseup", function(){
-        isDragging = false;
+        isDraggig = false;
     }).on("mousemove", function(event){
-        if(isDragging){
-            let offsetX = event.clientX - stratX;
-            let offsetY = event.clientY - stratY;
+        if (isDraggig) {
+            let offsetX = event.clientX - startX;
+            let offsetY = event.clientY - startY;
             $(".text-on-image").css({left: initialLeft + offsetX + "px", top: initialTop + offsetY + "px"});
         }
+    }).on("keydown", function(event){
+        if(event.ctrlKey && event.keyCode === 39){
+            $(".text-on-image").each(function(){
+                let rotation = ($(this).data("rotation") || 0) + 90;
+                $(this).css("transform", "rotate(" + rotation + "deg)").data("rotation", rotation);
+            });
+        }
+    });
+
+    $("#resetBtn, deleteBtn").click(function(){
+        $(".text-on-image").remove();
+        $(".textContainer").hide();
+        if (this.id == "deletBtn") $(".imgContainer").remove();
     })
 });
