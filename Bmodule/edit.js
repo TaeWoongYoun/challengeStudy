4738496a2eaa7e4907df734d9b2dae3671e41c65
xodnd0507 <24457545yong@gmail.com>
2024-03-27 22:49:51 +0900

    test : Study

--- Bmodule/edit.js
+++ Bmodule/edit.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("#addBtn").click(function(){
-        var input = $("<input>").attr({type: "file", accept: "image/*"}).on("change", function(event){
+        var input = $("<input>").attr({type: "file", accept : "image/*"}).on("change", function(event){
             var file = event.target.files[0];
             if (file) {
                 var reader = new FileReader();
@@ -12,15 +12,47 @@ $(document).ready(function(){
         });
         input.click();
     });
+    
     $("#textBtn").click(function(){
         $(".textContainer").show();
     });
+    
     $("#addTextBtn").click(function(){
         var text = $("#textBox").val().trim();
-        if (text !== ""){
+        if(text !== ""){
             $(".imgContainer").append($("<p>").addClass("text-on-image").text(text));
-            $(".textContainer").hide();
+            $(".textContainer").hide();    
         }
     });
-    $("#rotateBtn").
-});
+
+    $("#rotateBtn").click(function(){
+        $(".text-on-image").toggleClass("rotate");
+    })
+
+    $(document).on("mousedown", ".text-on-image", function(event){
+        isDragging = true;
+        startX = event.clientX;
+        startY = event.clientY;
+        initialLeft = $(this).position().left;
+        initialTop = $(this).position().top;
+    }).on("mouseup", function(){
+        isDragging = false;
+    }).on("mousemove", function(event){
+        if(isDragging){
+        let offsetX = event.clientX - startX;
+        let offsetY = event.clientY - startY;
+        $(".text-on-image").css({left:initialLeft + offsetX + "px", top: initialTop + offsetY +"px"});
+        }
+    }).on("keydown", function(event){
+        if(event.ctrlKey && event.keyCode === 39) {
+            let rotation = ($(this).data("rotation") || 0) + 90;
+            $(this).css("transform", "rotate(" + rotation + "deg)").data("rotation", rotation);
+        }
+    });
+
+    $("#resetBtn, #deleteBtn").click(function(){
+        $(".text-on-image").remove();
+        $(".textContainer").hide();
+        if(this.id === "deleteBtn") $(".imgContainer").remove();
+    })
+})
