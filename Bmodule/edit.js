dbc369440873d2642aceb684e217c28f5926e1a3
xodnd0507 <24457545yong@gmail.com>
2024-03-29 12:41:01 +0900

    fix : Update study and add file

--- Bmodule/edit.js
+++ Bmodule/edit.js
@@ -12,9 +12,11 @@ $(document).ready(function(){
         });
         input.click();
     });
+
     $("#textBtn").click(function(){
         $(".textContainer").show();
-    });
+    })
+
     $("#addTextBtn").click(function(){
         var text = $("#textBox").val().trim();
         if (text !== ""){
@@ -22,9 +24,11 @@ $(document).ready(function(){
             $(".textContainer").hide();
         }
     });
+
     $("#rotateBtn").click(function(){
         $(".text-on-image").toggleClass("rotate");
     });
+
     $(document).on("mousedown", ".text-on-image", function(event){
         isDragging = true;
         startX = event.clientX;
@@ -34,20 +38,23 @@ $(document).ready(function(){
     }).on("mouseup", function(){
         isDragging = false;
     }).on("mousemove", function(event){
-        if (isDragging) {
+        if(isDragging){
             let offsetX = event.clientX - startX;
             let offsetY = event.clientY - startY;
-            $(".text-on-image").css({left: initialLeft + offsetX + "px", top: initialTop + offsetY + "px"});
-        };
-    }).on("keydown", function(){
-        $(".text-on-image").each(function(){
-            let rotation = ($(this).data("rotation") || 0) + 90;
-            $(this).css("transform", "rotate(" + rotation + "deg)").data("rotation", rotation);
-        });
+            $(".text-on-image").css({left: initialLeft + offsetX + "px" , top: initialTop + offsetY + "px"});
+        }
+    }).on("keydown", function(event){
+        if (event.ctrlKey && event.keyCode === 39){
+            $(".text-on-image").each(function(){
+                let rotation = ($(this).data("rotation") || 0 ) + 90;
+                $(this).css("transform", "rotate(" + rotation + "deg)").data("rotation", rotation);
+            });
+        }
     });
+
     $("#resetBtn, #deleteBtn").click(function(){
         $(".text-on-image").remove();
         $(".textContainer").hide();
-        if (this.id === "deleteBtn") $(".imgContainer").remove();
+        if(this.id === "deleteBtn") $(".imgContainer").remove();
     })
 })
